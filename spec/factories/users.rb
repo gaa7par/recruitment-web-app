614caee2ce54b2a89556c8fb061693084c5c1624
gaa7par <gaa7par@gmail.com>
2017-02-15 14:58:17 +0100

    Add admin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     sequence(:email) { |n| "user-#{n}@app.com" }
     password 'secret'
     password_confirmation 'secret'
+
+    trait :admin do
+      admin true
+    end
   end
 end
