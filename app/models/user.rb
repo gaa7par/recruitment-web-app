b3e856e676bf9a6c0405c6a1f16801eaefdbed28
gaa7par <gaa7par@gmail.com>
2017-02-23 16:17:59 +0100

    Export to CSV

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.to_csv
+    attributes = %w(id email admin gender age)
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << user.attributes.values_at(*attributes)
+      end
+    end
+  end
 end
