22537e3463cae3e4fb312588ed8c87496738cf6e
gaa7par <gaa7par@gmail.com>
2017-02-24 16:37:28 +0100

    Add search field

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
 
     respond_to do |format|
       format.html
